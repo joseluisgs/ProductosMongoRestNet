65790a01d638c91ea9fedc7d41dbb3919963b71c
José Luis González <joseluisgonsan@gmail.com>
2024-09-17 10:04:44 +0200

    Configurando la conexión a MongoDB

--- ProductosMongoRestNet/Program.cs
+++ ProductosMongoRestNet/Program.cs
@@ -1,4 +1,6 @@
 using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using Serilog;
 using Serilog.Core;
 
@@ -16,6 +18,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+// Probamos mongoDB
+const string connectionUri = "mongodb+srv://joseluisgs:Mongo1234Ejemplo2024@cluster0.pgdqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+var settings = MongoClientSettings.FromConnectionString(connectionUri);
+// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+// Create a new client and connect to the server
+var client = new MongoClient(settings);
+// Send a ping to confirm a successful connection
+try {
+    var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+    Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+} catch (Exception ex) {
+    Console.WriteLine(ex);
 }
 
 // Usamos HTTPS redirection

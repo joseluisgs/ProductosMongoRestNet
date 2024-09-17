9385c6f9db182fbc349161ca20a20647ae1f3966
José Luis González Sánchez <joseluisgonsan@gmail.com>
2024-09-17 18:57:03 +0200

    Documentos y colecciones

---
+++ ProductosMongoRestNet/Database/BookDocument.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductosMongoRestNet.Models;
+
+public class BookDocument
+{
+    [BsonId] // Esto indica que el campo es el identificador de la colección
+    [BsonRepresentation(BsonType.ObjectId)] // Esto indica que el campo es de tipo ObjectId
+    public string? Id { get; set; } // El identificador de la colección
+
+    [BsonElement("Name")] // Esto indica el nombre del campo en la colección
+    public string BookName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
---
+++ ProductosMongoRestNet/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductosMongoRestNet.Models;
+
+public class Book
+{
+    public string? Id { get; set; }
+    
+    public string BookName { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; } = null!;
+
+    public string Author { get; set; } = null!;
+}
